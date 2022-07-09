2feab08fb5445ce04a0b9594b3c257cdea69fab4
charmii03 <charmigangani46@gmail.com>
2022-07-09 21:30:38 +0530

    js completed

---
+++ countHi.js
@@ -0,0 +1,10 @@
+function countHi(str){
+  if (str.length == 0){
+     return 0;
+   }
+ 
+   if (str.substring(0, 2) == "hi"){
+     return 1 + countHi(str.substring(2))
+   }
+   return countHi(str.substring(1));
+}
